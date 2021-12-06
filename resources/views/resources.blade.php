bcfe9cd249393098efc75d225303e43a1e6b7af0
TWS <sarbjitgrewal@tekkiwebsolutions.com>
2021-12-06 14:29:22 +0530

    resources page updated

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -33,7 +33,7 @@
                     </div>
                 </div>
                 <div class="row reports-page">
-                    <div class="col-lg-12 col-md-12 col-12 pb-4">
+                    <div class="col-lg-12 col-md-12 col-12 pb-4 report_page_leftpanel">
                         <div class="page-banner"><img alt="" class="img-fluid" src="images/resources-banner.jpg"></div>
                         <div class="row">
                             <div class="col-lg-9 col-md-9 col-12"><h3 class="resources-heading">Lorem ipsum dolor sir amet, consectetur adipiscing elit</h3></div>
@@ -41,12 +41,47 @@
                         </div>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+                    
+                    <div class="col-lg-12 col-sm-12 col-md-12 questionnaires_block">
+                        <form actoin='' method="post">
+                             <h3>Questionnaires</h3>
+                             <div class="suspend_top">
+                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec sapien enim.</span>
+                            <label><input type='checkbox' name="mycheckbox[]">Extremely Satisfied</label>
+                            <label><input type='checkbox'name="mycheckbox[]">Somewhat Satisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Neither Satisfied or dissatisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Somewhat dissatisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Extremely dissatisfied</label>
+                            </div>
+                            <div class="suspend_bottom">
+                            <span>Suspendisse nec sapien enim.</span>
+                            <label><input type='checkbox' name="mycheckbox[]">Extremely Satisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Somewhat Satisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Neither Satisfied or dissatisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Somewhat dissatisfied</label>
+                            <label><input type='checkbox' name="mycheckbox[]">Extremely dissatisfied</label>
+                            </div>
+                            <input type="submit" name='suspend_submit' value="Submit">
+                        </form>
                     </div>
-
+                     </div>   
                 </div>
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".questionnaires_block input").prop("checked", false);
+            $(".questionnaires_block input").click(function () {
+
+             if ($(this).is(":checked")) {
+             $(this).parent().addClass("selected");
+            } else {
+                $(this).parent().removeClass("selected");
+            }
+            })
+        })
+    </script>
 </div>
 
 @include('layouts.right_sidebar')
